eb4dbed2a493e89b16e4328496f9c541eac5f784
Fikri <fikrifadilah299@gmail.com>
2020-12-05 20:49:38 +0700

    portofolio

---
+++ webscraper.py
@@ -0,0 +1,20 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            print("\n" + str(url))
+           
+scrape = Scraper('https://liputan6.com/')
+scrape.scrape()
+
